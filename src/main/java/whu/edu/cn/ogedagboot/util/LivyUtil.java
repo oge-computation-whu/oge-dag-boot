b841d888129203401c70f2904600674809eae0bb
lrx <lrx_lucky@whu.edu.cn>
2023-03-02 20:10:46 +0800

    update LivyUtil.java 2023.03.02

--- src/main/java/whu/edu/cn/ogedagboot/util/LivyUtil.java
+++ src/main/java/whu/edu/cn/ogedagboot/util/LivyUtil.java
@@ -1,9 +1,12 @@
 package whu.edu.cn.ogedagboot.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import scala.annotation.meta.param;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import static whu.edu.cn.ogedagboot.util.HttpRequestUtil.sendGet;
@@ -13,78 +16,151 @@ import static whu.edu.cn.ogedagboot.util.SSHClientUtil.versouSshUtil;
 
 public class LivyUtil {
     public static void main(String[] args) {
-
-    }
-    public static String livyTrigger(String param) {
         String host = "125.220.153.26";
         String port = "19101";
         String baseUrl = "http://" + host + ":" + port;
+        String sessionInfoString = sendGet(baseUrl + "/sessions/");
+        JSONObject body = JSON.parseObject(sessionInfoString);
+        JSONArray jsonArray = body.getJSONArray("sessions");
+        JSONObject a = jsonArray.getJSONObject(0);
+        System.out.println("a = " + a);
+    }
+
+    public static void initLivy() {
+        String host = "125.220.153.26";
+        String port = "19101";
+        String baseUrl = "http://" + host + ":" + port;
+        int sessionNum = 6;
         try {
             versouSshUtil(host, "geocube", "ypfamily608", 22);
             String st =
-                    "cd /home/geocube/oge" + "\n" + "rm -rf on-the-fly" + "\n" + "mkdir on-the-fly" + "\n" +
-                    "cd /home/geocube/oge/oge-server/dag-boot" + "\n" + "rm -rf webapi" + "\n" + "mkdir webapi" + "\n" +
-                    "cd /home/geocube/tomcat8/apache-tomcat-8.5.57/webapps/" + "\n" + "rm -rf webapi" + "\n" + "mkdir webapi" + "\n";
+                    "cd /home/geocube/livy/bin/" + "\n" + "./livy-server stop" + "\n" + "./livy-server start" + "\n";
             System.out.println("st = " + st);
             runCmd(st, "UTF-8");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        int sessionNum = 6;
-        String jsonString = param;
-        Long time = System.currentTimeMillis();
+        for (int i = 0; i < sessionNum; i++) {
+            JSONObject body = new JSONObject();
+            body.put("name", "spark" + i);
+            body.put("kind", "spark");
+            body.put("executorCores", 8);
+            body.put("executorMemory", "3g");
+            String[] str = {"local:/home/geocube/oge/oge-server/dag-boot/oge-computation_ogc_on_the_fly.jar"};
+            body.put("jars", str);
+            JSONObject bodyChildren = new JSONObject();
+            bodyChildren.put("spark.driver.extraClassPath", "local:/home/geocube/spark/jars/*");
+            bodyChildren.put("spark.executor.extraClassPath", "local:/home/geocube/spark/jars/*");
+            body.put("conf", bodyChildren);
+            String param = body.toJSONString();
+            sendPost(baseUrl + "/sessions", param);
+        }
+    }
+
+    public static String livyTrigger(String param) {
+        String host = "125.220.153.26";
+        String port = "19101";
+        String baseUrl = "http://" + host + ":" + port;
+        int sessionNumExpected = 6;
+        try {
+            versouSshUtil(host, "geocube", "ypfamily608", 22);
+            String st =
+                    "cd /home/geocube/oge" + "\n" + "rm -rf on-the-fly" + "\n" + "mkdir on-the-fly" + "\n" +
+                            "cd /home/geocube/oge/oge-server/dag-boot" + "\n" + "rm -rf webapi" + "\n" + "mkdir webapi" + "\n" +
+                            "cd /home/geocube/tomcat8/apache-tomcat-8.5.57/webapps/" + "\n" + "rm -rf webapi" + "\n" + "mkdir webapi" + "\n";
+            System.out.println("st = " + st);
+            runCmd(st, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long time = System.currentTimeMillis();
         String fileNameJson = "/home/geocube/oge/oge-server/dag-boot/on-the-fly/outputjson_" + time + ".txt";
         String fileName = "/home/geocube/oge/oge-server/dag-boot/on-the-fly/output_" + time + ".txt";
         File writeFile = new File(fileNameJson);
         Writer writer = null;
         try {
             writer = new BufferedWriter(new FileWriter(writeFile));
-            writer.write(jsonString);
+            writer.write(param);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        int sessionId = -1;
-        for(int i = 0; i < sessionNum; i++){
-            String sessionInfoString = sendGet(baseUrl + "/sessions/" + i);
+        //获取所有的session
+        String allSessionInfoString = sendGet(baseUrl + "/sessions/");
+        JSONObject allSessionInfoObject = JSON.parseObject(allSessionInfoString);
+        int sessionNum = Integer.parseInt(allSessionInfoObject.getString("total"));
+        JSONArray sessionArray = allSessionInfoObject.getJSONArray("sessions");
+        ArrayList<Integer> sessionIdList = new ArrayList<>();
+        for (int i = 0; i < sessionArray.size(); i++) {
+            JSONObject sessionInfoObject = sessionArray.getJSONObject(i);
+            int sessionId = Integer.parseInt(sessionInfoObject.getString("id"));
+            sessionIdList.add(sessionId);
+        }
+
+        //已经挂掉的session重新启动
+        if (sessionNum < sessionNumExpected) {
+            for (int i = 0; i < sessionNumExpected - sessionNum; i++) {
+                JSONObject body = new JSONObject();
+                body.put("name", "spark" + (sessionIdList.get(sessionIdList.size() - 1) + 1));
+                body.put("kind", "spark");
+                body.put("executorCores", 8);
+                body.put("executorMemory", "3g");
+                String[] str = {"local:/home/geocube/oge/oge-server/dag-boot/oge-computation_ogc_on_the_fly.jar"};
+                body.put("jars", str);
+                JSONObject bodyChildren = new JSONObject();
+                bodyChildren.put("spark.driver.extraClassPath", "local:/home/geocube/spark/jars/*");
+                bodyChildren.put("spark.executor.extraClassPath", "local:/home/geocube/spark/jars/*");
+                body.put("conf", bodyChildren);
+                String paramRequest = body.toJSONString();
+                sendPost(baseUrl + "/sessions", paramRequest);
+            }
+        }
+
+        //检查session能否使用
+        int sessionIdAvailable = -1;
+        for (int i = 0; i < sessionIdList.size(); i++) {
+            String sessionInfoString = sendGet(baseUrl + "/sessions/" + sessionIdList.get(i));
             JSONObject sessionInfoJson = JSON.parseObject(sessionInfoString);
-            if(Objects.equals(sessionInfoJson.getString("state"), "idle")){
-                sessionId = i;
+            if (Objects.equals(sessionInfoJson.getString("state"), "idle")) {
+                sessionIdAvailable = sessionIdList.get(i);
                 break;
             }
         }
-        if(sessionId == -1){
-            String statementsInfoString = sendGet(baseUrl + "/sessions/0/statements");
+
+        //如果没有一个session是闲置的，则取消第一个session正在执行的任务
+        if (sessionIdAvailable == -1) {
+            String statementsInfoString = sendGet(baseUrl + "/sessions/" + sessionIdList.get(0) + "/statements");
             JSONObject statementsInfoJson = JSON.parseObject(statementsInfoString);
             int totalStatements = statementsInfoJson.getInteger("total_statements");
-            String cancelInfoString = sendPost(baseUrl + "/sessions/0/statements/" + (totalStatements - 1),"");
+            String cancelInfoString = sendPost(baseUrl + "/sessions/" + sessionIdList.get(0) + "/statements/" + (totalStatements - 1), "");
             JSONObject cancelInfoJson = JSON.parseObject(cancelInfoString);
             String cancelBool = cancelInfoJson.getString("msg");
-            if(Objects.equals(cancelBool, "canceled")){
-                System.out.println("Already Cancel Session " + 0 + " And Statement " + (totalStatements - 1));
+            if (Objects.equals(cancelBool, "canceled")) {
+                System.out.println("Already Cancel Session " + sessionIdList.get(0) + " And Statement " + (totalStatements - 1));
             }
-            sessionId = 0;
+            sessionIdAvailable = sessionIdList.get(0);
         }
 
+        //提交任务给session
         JSONObject body = new JSONObject();
         String code = "whu.edu.cn.application.oge.Trigger.runMain(sc,\"" + fileNameJson + "\",\"" + fileName + "\")";
         body.put("code", code);
         body.put("kind", "spark");
         String parameter = body.toJSONString();
-        String outputString = sendPost(baseUrl + "/sessions/" + sessionId + "/statements", parameter);
+        String outputString = sendPost(baseUrl + "/sessions/" + sessionIdAvailable + "/statements", parameter);
         System.out.println("outputString = " + outputString);
         JSONObject jsonObject = JSON.parseObject(outputString);
         int statementId = jsonObject.getInteger("id");
-        while(true) {
-            String statementInfoString = sendGet(baseUrl + "/sessions/" + sessionId + "/statements/" + statementId);
+        while (true) {
+            String statementInfoString = sendGet(baseUrl + "/sessions/" + sessionIdAvailable + "/statements/" + statementId);
             JSONObject statementInfoJson = JSON.parseObject(statementInfoString);
             String state = statementInfoJson.getString("state");
-            if(Objects.equals(state, "available")){
+            if (Objects.equals(state, "available")) {
                 break;
             }
-            if(Objects.equals(state, "error")){
-                throw new RuntimeException("Submit Session "+ sessionId + " And Statement " + statementId + " Error!!!");
+            if (Objects.equals(state, "error")) {
+                throw new RuntimeException("Submit Session " + sessionIdAvailable + " And Statement " + statementId + " Error!!!");
             }
         }
 
