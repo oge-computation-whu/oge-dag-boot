d2ca36b99be03f37747c490a41879db17082acd5
forDecember <1921567337@qq.com>
2023-07-14 17:11:46 +0800

    update: deliverNotice 回调

--- src/main/java/whu/edu/cn/ogedagboot/controller/CallbackController.java
+++ src/main/java/whu/edu/cn/ogedagboot/controller/CallbackController.java
@@ -4,8 +4,8 @@ package whu.edu.cn.ogedagboot.controller;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * 交给 oge_computation_ogc 使用的回调服务
@@ -15,7 +15,11 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CallbackController {
 
     // 不同用户（或不同原始请求）下 TMS 服务输出的json
-    public static final ConcurrentHashMap<String, String> outJsonsOfTMS = new ConcurrentHashMap<>();
+    public static final
+    ConcurrentHashMap<String, String> outJsonsOfTMS = new ConcurrentHashMap<>();
+
+    public static final
+    ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> noticeJsonMap = new ConcurrentHashMap<>();
 
 
     /**
@@ -25,9 +29,39 @@ public class CallbackController {
      */
     @PostMapping("/deliverUrl")
     public void deliverUrl(@RequestBody String outJson) {
-        outJsonsOfTMS.put(JSONObject.parseObject(outJson).getString("workID"),
-                JSONObject.parseObject(outJson).getJSONObject("json").toJSONString());
+        outJsonsOfTMS.put(
+                JSONObject.parseObject(outJson).getString("workID"),
+                JSONObject.parseObject(outJson).getJSONObject("json").toJSONString()
+        );
         System.out.println("outJsonsOfTMS = " + outJsonsOfTMS);
         System.out.println("url = " + outJson);
     }
+
+
+    @PostMapping("/deliverNotice")
+    public void deliverNotice(@RequestBody String noticeJson) {
+
+        String workID = JSONObject
+                .parseObject(noticeJson)
+                .getString("workID");
+        String notice = JSONObject
+                .parseObject(noticeJson)
+                .getJSONObject("notice").toJSONString();
+        // springboot controller 默认为单例
+        synchronized (this) {
+            if (!noticeJsonMap.containsKey(workID)) {
+                ConcurrentLinkedQueue<String> noticeQueue = new ConcurrentLinkedQueue<>();
+                noticeQueue.add(notice);
+                noticeJsonMap.put(workID, noticeQueue);
+            } else {
+                noticeJsonMap.get(workID).add(notice);
+            }
+        }
+
+        return;
+
+
+    }
+
+
 }
